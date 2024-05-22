4f1b1cc62b1b67cce0d96c7d0aecb0d61e08ffb2
e <120667897+ninja888-8@users.noreply.github.com>
2024-05-21 20:11:24 -0400

    Update SplashScreen.java

--- SplashScreen.java
+++ SplashScreen.java
@@ -1,43 +1,82 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
 
-//canvas is similar to creating graphics window in Processing.
-//JPanel is the container which holds all gui objects and must be created first
-public class SplashScreen extends JPanel
-{
-  private BufferedImage logo;
-  private BufferedImage play;
-  private BufferedImage exit;
-  Color color = new Color(245, 228, 255);
+public class SplashScreen extends JComponent {
+   private BufferedImage logo;
+   private BufferedImage play;
+   private BufferedImage play2;
+   private BufferedImage exit;
+   private BufferedImage exit2;
+   private boolean selected;
+   private int choice = -1;
+   private Color bg;
 
-  public SplashScreen(GridLayout gl)
-  {
-    this.setLayout(gl);
-    try {
-      logo = ImageIO.read(new File("logo.png"));
-    }
-    catch (IOException ioe) {
-    
-    }
-  }
+   public SplashScreen() {
+      this.addKeyListener(new KeyHandler());
+      try {
+         logo = ImageIO.read(new File("logo.png"));
+         play = ImageIO.read(new File("playButton.png"));
+         play2 = ImageIO.read(new File("playButton2.png"));
+         exit = ImageIO.read(new File("exitButton.png"));
+         exit2 = ImageIO.read(new File("exitButton2.png"));
+      }
+      catch (IOException ioe) {
+         System.out.println("Missing image file.");
+      }
+      selected = true; // on the first button
+      bg = new Color(245, 228, 255);
+   }
+   
+   private class KeyHandler extends KeyAdapter {
+      public void keyPressed(KeyEvent e) {
+         int key = e.getKeyCode();
+         
+         if (key == KeyEvent.VK_DOWN)
+            selected = false;
+         else if (key == KeyEvent.VK_UP)
+            selected = true;
+         else if (key == KeyEvent.VK_ENTER)
+            choice = selected ? 0 : 1;
+            
+         System.out.println(choice);
+         SplashScreen.this.repaint();
+      }
+   }
+   
+   public void paintComponent(Graphics g) {
+      g.setColor(bg);
+      g.fillRect(0, 0, 810, 1080);
+      int w = getWidth();
+      int h = getHeight();
 
-  public void paintComponent(Graphics g)
-  {
-    setBackground(color);
-    super.paintComponent(g);
-    int w = getWidth();
-    int h = getHeight();
-
-    int x0 = 0;
-    int y0 = 200;
-    g.drawImage(logo, 100, 100, this);
-    
-    g.setColor(Color.black);
-    g.setFont(new Font("SansSerif", Font.PLAIN, 50));
-    g.drawString("Chat-Mod AI Inc.", x0, y0);
-  }
+      g.drawImage(logo, 245, 100, this);
+      
+      g.setColor(Color.black);
+      g.setFont(new Font("Calibri", Font.BOLD, 50));
+      g.drawString("Chat-Mod AI Inc.", 235, 520);
+      
+      if (selected) {
+         System.out.println("wee");
+         g.drawImage(play2, 255, 600, this);
+         g.drawImage(exit, 255, 720, this);
+      }
+      else {
+         System.out.println("woo");
+         g.drawImage(play, 255, 600, this);
+         g.drawImage(exit2, 255, 720, this);
+      }
+      
+      g.setFont(new Font("Calibri", Font.BOLD, 64));
+      g.drawString("Use Arrow Keys and press", 50, 900);
+      g.drawString("‘Enter’ to Continue.", 135, 970);
+   }
+   
+   public int getChoice() {
+      return choice;
+   }
 }
 
