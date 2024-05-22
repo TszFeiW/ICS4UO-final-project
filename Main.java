ae4aa6aecd666986a31b7c1568040d9d202ca8a6
e <120667897+ninja888-8@users.noreply.github.com>
2024-05-21 20:11:47 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,30 @@
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Main {
-
-  public static void main(String[] args) {
-    JFrame window = new JFrame("CMod Socializer");
-    window.setSize(1920, 1080);
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    SplashScreen ss = new SplashScreen(new GridLayout(5,1));
-    window.getContentPane().add(ss);
-    window.setVisible(true);
-  }
+   
+   public static void main(String[] args) throws InterruptedException {
+      JFrame window = new JFrame("CMod Socializer");
+      window.setSize(810, 1080);
+      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      SplashScreen ss = new SplashScreen();
+      window.getContentPane().add(ss);
+      ss.setFocusable(true);
+      ss.requestFocusInWindow();
+      window.setVisible(true);
+      while (true) { // until the user makes a choice
+         if (ss.getChoice() != -1) break;
+      }
+      if (ss.getChoice() == 1) { // chooses to exit
+         window.setVisible(false);
+      }
+      window.getContentPane().remove(ss);
+      
+      
+   }
   
 }
 
