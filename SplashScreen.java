e9206895e5c7c4b2da5b57f58bd25e4f01e50ad4
e <120667897+ninja888-8@users.noreply.github.com>
2024-05-23 19:04:23 -0400

    Update SplashScreen.java

--- SplashScreen.java
+++ SplashScreen.java
@@ -1,3 +1,15 @@
+/**
+ * This class is used as the splash screen for our game.
+ * @version 1
+ * May 24th, 2024
+ * Time Spent: 3 hours
+ * @author Eric Ning, Tsz Fei Wang
+ *
+ * Modifications: Class was created which runs the splash screen of our game. Keyboard input is used
+ *                to detect whether the user wishes to continue or wishes to leave the game. 
+ * 
+ */
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -6,6 +18,17 @@ import java.io.*;
 import javax.imageio.ImageIO;
 
 public class SplashScreen extends JComponent {
+
+   /**
+    * private BufferedImage logo  - image of the logo
+    * private BufferedImage play  - image of the non-selected play button
+    * private BufferedImage play2 - image of the selected play button
+    * private BufferedImage exit  - image of the non-selected exit button
+    * private BufferedImage exit2 - image of the selected exit button
+    * private int selected        - the current button that is selected (numbered 0 to 3)
+    * private int choice          - the selected button
+    * private Color bg            - the color of the background
+    */
    private BufferedImage logo;
    private BufferedImage play;
    private BufferedImage play2;
@@ -15,6 +38,9 @@ public class SplashScreen extends JComponent {
    private int choice = -1;
    private Color bg;
 
+   /**
+    * Constructor of the class so that an instance of the class can be created in Main
+    */
    public SplashScreen() {
       this.addKeyListener(new KeyHandler());
       try {
@@ -31,7 +57,14 @@ public class SplashScreen extends JComponent {
       bg = new Color(245, 228, 255);
    }
    
+   /**
+    * This private class extends KeyAdapter so the drawing can detect key inputs
+    */
    private class KeyHandler extends KeyAdapter {
+      /**
+       * This method allows the actual game to run (main method)   
+       * @param KeyEvent e An event that shows that a keyboard input as been made
+       */
       public void keyPressed(KeyEvent e) {
          int key = e.getKeyCode();
          
@@ -46,6 +79,10 @@ public class SplashScreen extends JComponent {
       }
    }
    
+   /**
+    * This method is capable of actually drawing onto the JFrame window.
+    * @param Graphics g An object which is a painting tool
+    */
    public void paintComponent(Graphics g) {
       g.setColor(bg);
       g.fillRect(0, 0, 810, 1080);
@@ -70,6 +107,10 @@ public class SplashScreen extends JComponent {
       g.drawString("‘Enter’ to Continue.", 135, 970);
    }
    
+   /**
+    * This method allows the Main class to access the choice that the user made 
+    * @return The choice that the user made in the menu
+    */
    public int getChoice() {
       return choice;
    }
