685773df24ab2460ae87d0effaef6242e658f3b1
e <120667897+ninja888-8@users.noreply.github.com>
2024-05-27 22:55:56 -0400

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -20,6 +20,7 @@ import javax.swing.JOptionPane;
 public class Level1 extends JComponent {
    private BufferedImage user; 
    private String username; 
+   private boolean finished;
    Color bg; 
    
    public Level1() {
@@ -33,62 +34,71 @@ public class Level1 extends JComponent {
       }
    }
    
-   public void paintComponent(Graphics g) {
-      try {
-         g.drawImage(user, 0, 0, this); 
-         
-         char c; 
-         while(true) {
-            c = KeyEvent.KEY_TYPED; 
-            if (c == '\n' && username.length() >= 1) break;
-            if (c == 8) {
-               if (username.length() == 0) {
-                  JOptionPane.showMessageDialog(null, "There are no characters to delete.", "Error", JOptionPane.WARNING_MESSAGE);
-                  continue;
-               } 
-               else {
-                     username = username.substring(0, username.length() - 1);
-                     g.setColor(bg);
-                     g.fillRect(500, 420, 350, 100);
-                     g.setColor(Color.black);
-                     g.drawString(username, 480, 478);
-                     continue;
-               }
-            }
-            while (!Character.isLetterOrDigit(c)) {
-                JOptionPane.showMessageDialog(null, "Please only use alphanumerical characters.", "Error", JOptionPane.WARNING_MESSAGE);
-                c = KeyEvent.KEY_TYPED;
-            }
-
-             if (c == '\n' && username.length() < 1) {
-                 JOptionPane.showMessageDialog(null, "Username too short!", "Error", JOptionPane.WARNING_MESSAGE);
-                 continue;
-             }
-            
-             if(username.length() == 20) {
-                 JOptionPane.showMessageDialog(null, "Username has reached maximum length.!", "Error", JOptionPane.WARNING_MESSAGE);
-             }
-             else if (c != 8 && c != '\n') username += c;
-             g.setColor(bg);
-             g.fillRect(520, 520, 350, 100);
-             g.setColor(Color.black);
-             g.drawString(username, 480, 478);
-         }
-      }
-         catch (Exception e) { }
-      }
-      
-      private class KeyHandler extends KeyAdapter {
+   /**
+    * This private class extends KeyAdapter so the drawing can detect key inputs
+    */
+   private class KeyHandler extends KeyAdapter {
       /**
+       * This method allows the actual game to run (main method)   
        * @param KeyEvent e An event that shows that a keyboard input as been made
        */
       public void keyPressed(KeyEvent e) {
-         int key = e.getKeyCode();         
+         int key = e.getKeyCode();
+         
+         //if (key == KeyEvent.VK_ENTER) currScreen++;
+         //if (currScreen == 3) finished = true;
+            
          Level1.this.repaint();
       }
+   }
+   
+   public void paintComponent(Graphics g) {
+      g.drawImage(user, 0, 0, this); 
       
-      public String getUsername() {
-         return username;
+      char c; 
+      while(true) {
+         c = KeyEvent.KEY_TYPED; 
+         if (c == '\n' && username.length() >= 1) break;
+         if (c == 8) {
+            if (username.length() == 0) {
+               JOptionPane.showMessageDialog(null, "There are no characters to delete.", "Error", JOptionPane.WARNING_MESSAGE);
+               continue;
+            } 
+            else {
+                  username = username.substring(0, username.length() - 1);
+                  g.setColor(bg);
+                  g.fillRect(500, 420, 350, 100);
+                  g.setColor(Color.black);
+                  g.drawString(username, 480, 478);
+                  continue;
+            }
+         }
+         while (!Character.isLetterOrDigit(c)) {
+             JOptionPane.showMessageDialog(null, "Please only use alphanumerical characters.", "Error", JOptionPane.WARNING_MESSAGE);
+             c = KeyEvent.KEY_TYPED;
+         }
+
+          if (c == '\n' && username.length() < 1) {
+              JOptionPane.showMessageDialog(null, "Username too short!", "Error", JOptionPane.WARNING_MESSAGE);
+              continue;
+          }
+         
+          if(username.length() == 20) {
+              JOptionPane.showMessageDialog(null, "Username has reached maximum length.!", "Error", JOptionPane.WARNING_MESSAGE);
+          }
+          else if (c != 8 && c != '\n') username += c;
+          g.setColor(bg);
+          g.fillRect(520, 520, 350, 100);
+          g.setColor(Color.black);
+          g.drawString(username, 480, 478);
       }
+  }
+      
+   public String getUsername() {
+      return username;
+   }
+   
+   public boolean getFinished() {
+      return finished;
    }
 }
