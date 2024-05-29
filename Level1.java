c43662a9925e0bdf84b1c15f5bfa2a9453721765
e <120667897+ninja888-8@users.noreply.github.com>
2024-05-28 23:50:12 -0400

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -21,9 +21,11 @@ public class Level1 extends JComponent {
    private BufferedImage user; 
    private String username = ""; 
    private boolean finished = false;
+   private boolean isUsername = true;
    private Color bg; 
-   private long lastTime = 0; 
-   private static final long INTERVAL = (long)500;
+   private char ch = 'x';
+   //private long lastTime = 0; 
+   //private static final long INTERVAL = (long)500;
    public Level1() {
       this.addKeyListener(new KeyHandler());
       try {
@@ -44,15 +46,21 @@ public class Level1 extends JComponent {
        * @param KeyEvent e An event that shows that a keyboard input as been made
        */
       public void keyPressed(KeyEvent e) {
-         int key = e.getKeyCode();
+         if (isUsername) {
+            
+            ch = e.getKeyChar();
+            System.out.println("char: " + ch);
+            Level1.this.repaint();
+         }
          //if (key == KeyEvent.VK_ENTER) currScreen++;
          //if (currScreen == 3) finished = true;
          
       }
-      
+      /*
       public void keyReleased(KeyEvent e) {
          long timePressed = e.getWhen(); 
          long diff = timePressed - lastTime; 
+         System.out.println("hihihi");
          if(diff > INTERVAL) {
             char c = e.getKeyChar();
             username = username + c; 
@@ -62,56 +70,68 @@ public class Level1 extends JComponent {
          }
          lastTime = timePressed; 
       }
+      */
    }
    
    public void paintComponent(Graphics g) {
-      g.drawImage(user, 0, 0, this);
-      char c = username.charAt(username.length()-1); 
-      int temp = 0; 
-        
-      while(true) {
-         if (c == '\n' && username.length() >= 1) { 
-            username = username.substring(0, username.length()-1); 
-            break;
-         }
-         if (c == 8) {
-            if (username.length() == 1) {
-               JOptionPane.showMessageDialog(null, "There are no characters to delete.", "Error", JOptionPane.WARNING_MESSAGE);
-               username = ""; 
-               continue;
-            } 
-            else {
-               username = username.substring(0, username.length() - 2);
-               g.setColor(bg);
-               g.fillRect(500, 420, 350, 100);
-               g.setColor(Color.black);
-               g.drawString(username, 480, 478);
-               continue;
+      g.setColor(bg);
+      g.fillRect(0, 0, 810, 1080);
+      if (isUsername) {
+         g.drawImage(user, 0, 0, this);
+         char c = 'x';
+         int temp = 0; 
+         System.out.println("username: " + username);
+         try {Thread.sleep(100);} catch (InterruptedException ie) {}
+            c = ch;
+            ch = 'x';
+            System.out.println(ch);
+            if (c == 'x') return;
+            
+            
+            if (c == '\n' && username.length() >= 1) { 
+               isUsername = false;
+               return;
             }
-         }
-         while (!Character.isLetterOrDigit(c)) {
-             JOptionPane.showMessageDialog(null, "Please only use alphanumerical characters.", "Error", JOptionPane.WARNING_MESSAGE);
-             username = username.substring(0, username.length()-1);
-         }
-          if (c == '\n' && username.length() < 2) {
-              JOptionPane.showMessageDialog(null, "Username too short!", "Error", JOptionPane.WARNING_MESSAGE);
-              username = username.substring(0, username.length()-1);
-              temp++; if(temp > 5) break; 
-              continue;
-          }
-         
-          if(username.length() > 20) {
-              JOptionPane.showMessageDialog(null, "Username has reached maximum length!", "Error", JOptionPane.WARNING_MESSAGE);
-              username.substring(0, username.length()-1);
-              temp++; if(temp > 5) break; 
-              continue;
-          }
-          if (c != 8 && c != '\n') continue;
-          c = ' '; 
-          g.setColor(bg);
-          g.fillRect(520, 520, 350, 100);
-          g.setColor(Color.black);
-          g.drawString(username, 480, 478);
+            if (c == 8) {
+               if (username.length() == 0) {
+                  JOptionPane.showMessageDialog(null, "There are no characters to delete.", "Error", JOptionPane.WARNING_MESSAGE);
+                  username = ""; 
+                  c = 'x';
+                  return;
+               } 
+               else {
+                  username = username.substring(0, username.length() - 1);
+                  g.setColor(bg);
+                  g.fillRect(500, 420, 350, 100);
+                  g.setColor(Color.black);
+                  g.drawString(username, 480, 478);
+                  c = 'x';
+                  return;
+               }
+            }
+            if (!Character.isLetterOrDigit(c)) {
+                JOptionPane.showMessageDialog(null, "Please only use alphanumerical characters.", "Error", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+             if (c == '\n' && username.length() < 2) {
+                 JOptionPane.showMessageDialog(null, "Username too short!", "Error", JOptionPane.WARNING_MESSAGE);
+                 temp++; if(temp > 5) isUsername = false; 
+                 return;
+             }
+            
+             if(username.length() >= 20) {
+                 JOptionPane.showMessageDialog(null, "Username has reached maximum length!", "Error", JOptionPane.WARNING_MESSAGE);
+                 temp++; if(temp > 5) isUsername = false;
+                 return; 
+             }
+             username += c;
+             
+             g.setColor(bg);
+             g.fillRect(520, 520, 350, 100);
+             g.setColor(Color.black);
+             g.drawString(username, 480, 478);
+             
+             c = 'x';
       }
   }
       
