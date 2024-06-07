07e7700d82c7fc2b3a2c40f12c23f479c4cb24ea
fief <109440858+TszFeiW@users.noreply.github.com>
2024-06-06 20:12:04 -0400

    Add files via upload

--- Credits.java
+++ Credits.java
@@ -5,32 +5,39 @@
  * Version 1.0 
  * Time Spent: 1 hour
  * Class was created which runs the credits screen of our game. Keyboard input
- * is used to detect whether the user wishes to exit the game yet 
+ * is used to detect whether the user wishes to exit the game yet .
  * </p>
  *
  * <p>
  * Version 1.1
  * Time Spent: < 1 hour
- * Comments modified
+ * Comments modified.
  * </p>
  * 
  * <p>
  * Version 1.2
  * Time Spent: < 1 hour
- * Coordinates of some drawings adjusted so that it fits on school monitor
+ * Coordinates of some drawings adjusted so that it fits on school monitor.
  * </p>
  *
  * <p>
  * Version 1.3
  * Time Spent: 1 hour
- * Added an animation to end the credits screen
+ * Added an animation to end the credits screen.
+ * </p>
+ *
+ * <p>
+ * Version 1.4
+ * Time Spent: 1 hour 
+ * Program was modified so that it implements Runnable (fixes a few bugs in the program).
+ * Comments modified.
  * </p>
  *
  * @author Tsz Fei Wang
- * @version 1.3
+ * @version 1.4
  * 
  * Chat-Mod AI Inc.
- * June 5th, 2024
+ * June 6th, 2024
  */
 
 import java.awt.*;
@@ -40,7 +47,7 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
 
-public class Credits extends JComponent {
+public class Credits extends JComponent implements Runnable {
 
    /**
     * private BufferedImage logo  - image of the logo
@@ -61,13 +68,17 @@ public class Credits extends JComponent {
     * Constructor of the class so that an instance of the class can be created in Main
     */
    public Credits() {
-      this.addKeyListener(new KeyHandler());
+      this.addKeyListener(new KeyHandler()); // adding KeyListener
+      
       try {
+         // importing logo image
          logo = ImageIO.read(new File("logo.png"));
       }
       catch (IOException ioe) {
          System.out.println("Missing image file.");
       }
+      
+      // initializing other instance variables
       selected = false; // chosen to exit or not
       bg = new Color(224, 240, 244);
       bg2 = new Color(245, 228, 255);
@@ -84,7 +95,7 @@ public class Credits extends JComponent {
       public void keyPressed(KeyEvent e) {
          int key = e.getKeyCode();
          
-         if (key == KeyEvent.VK_ENTER)
+         if (key == KeyEvent.VK_ENTER) // pressed enter: user has chosen to exit the game entirely
             selected = true;
          
          Credits.this.repaint();
@@ -122,6 +133,7 @@ public class Credits extends JComponent {
       g.drawString("Press ‘Enter’ to Exit.", 135, 400);
       
       if (selected && counter <= 398) { // if the user has chosen to exit and animation is ongoing
+         // adds thicker and thicker black borders until the screen is completely black
          g.setColor(Color.black);
          g.fillRect(0, 0, counter, 980);
          g.fillRect(0, 0, 795, counter);
@@ -133,15 +145,23 @@ public class Credits extends JComponent {
       }
       else if (selected) { // animation finished 
          finished = true;
+         g.setColor(Color.black);
+         g.fillRect(0, 0, 810, 1020);
       }
    }
    
    /**
-    * This method allows the Main class to see if the animation has finished
-    * @return Whether is program is finished
+    * Method that allows threads to be run (starts a new thread)
     */
-   public boolean getFinished() {
-      return finished;
+   public void run() {
+      try {
+         while (true) {
+            Thread.sleep(500);
+            if (finished) break; // until the credits screen is complete 
+         }
+      } catch (InterruptedException ie) {
+         System.out.println("InterruptedException has occured.");
+      }
    }
 }
 
