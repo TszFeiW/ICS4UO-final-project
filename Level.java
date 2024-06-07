30d2489ba7b1f10fc6b5365e3da870d3bdefc813
fief <109440858+TszFeiW@users.noreply.github.com>
2024-06-06 20:09:42 -0400

    Add files via upload

--- Level.java
+++ Level.java
@@ -1,23 +1,30 @@
 /**
- * This abstract class is used as a template for our Level1 and Level2 classes
+ * This abstract class is used as a template for our Level1 and Level2 classes.
  * 
  * <p>
  * Version 1.0 
  * Time Spent: 1 hour
- * Class was created as a template for the levels in our game
+ * Class was created as a template for the levels in our game.
  * </p>
  *
  * <p>
  * Version 1.1
  * Time Spent: 1 hour
- * Class modified so that it is fully commented and functional
+ * Class modified so that it is fully commented and functional.
+ * </p>
+ * 
+ * <p>
+ * Version 1.2
+ * Time Spent: 1 hour 
+ * Program was modified so that it implements Runnable (fixes a few bugs in the program).
+ * Comments modified.
  * </p>
  *
  * @author Eric Ning, Tsz Fei Wang
- * @version 1.1
+ * @version 1.2
  * 
  * Chat-Mod AI Inc.
- * June 5th, 2024
+ * June 6th, 2024
  */
 
 import java.awt.*;
@@ -25,7 +32,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 
-abstract class Level extends JComponent {
+abstract class Level extends JComponent implements Runnable {
    
    /*
     * protected String username  - the user's username
@@ -43,6 +50,7 @@ abstract class Level extends JComponent {
     * @param Color bg The color of the background
     */
    public Level(String username, Color bg) {
+      // initializing instance variables
       this.username = username;
       this.bg = bg;
       finished = false; // level has not been finished at the time of initialization
@@ -67,10 +75,16 @@ abstract class Level extends JComponent {
    abstract void displayTransition(Graphics g);
    
    /**
-    * This method allows the Main class to access whether the user is done the level or not
-    * @return Whether the user has finished the level
+    * Method that allows threads to be run (starts a new thread)
     */
-   public boolean getFinished() {
-      return finished;
+   public void run() {
+      try {
+         while (true) {
+            Thread.sleep(500);
+            if (finished) break; // until the level is complete
+         }
+      } catch (InterruptedException ie) {
+         System.out.println("InterruptedException has occured.");
+      }
    }
 }
